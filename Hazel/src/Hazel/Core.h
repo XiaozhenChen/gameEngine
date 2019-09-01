43da2bee7ea40665ead85c4befd0b8016992730c
XiaozhenChen <skycxz@qq.com>
2019-09-01 17:20:34 +0800

    Set up Engine Entry Point

---
+++ Hazel/src/Hazel/Core.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+#ifdef HZ_PLATFORM_WINDOWS
+#ifdef HZ_BUILD_DLL
+  #define HAZEL_API __declspec(dllexport)
+#else 
+  #define HAZEL_API __declspec(dllimport)
+#endif // HZ_BUILD_DLL                
+
+#else
+#error Hazal only supports Windows!
+
+#endif
+
+
+
+
+
---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef  HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+
+int main(int argc,char** argv )
+{
+	printf("Start Engine");
+	auto app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+	
+}
+
+
+
+#endif //  HZ_PLATFORM_WINDOWS
