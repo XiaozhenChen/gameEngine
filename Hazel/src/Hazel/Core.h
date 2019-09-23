e97681f61adefd60f9582bf13b17c75d87422e39
XiaozhenChen <skycxz@qq.com>
2019-09-23 16:17:40 +0800

    add glad gui key img layer

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,13 +1,11 @@
 #pragma once
 
-
 #ifdef HZ_PLATFORM_WINDOWS
 #ifdef HZ_BUILD_DLL
   #define HAZEL_API __declspec(dllexport)
 #else 
   #define HAZEL_API __declspec(dllimport)
 #endif // HZ_BUILD_DLL                
-
 #else
 #error Hazal only supports Windows!
 
@@ -21,6 +19,7 @@
 #define HZ_CORE_ASSERT(x, ...)
 #endif
 
-#define BIT(x) (1 << x) 
+#define BIT(x) (1 << x)
+#define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1) 
 
 
