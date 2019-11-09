1415a63dbe6c3d6da98b0e8ea4ed88f4354d6d49
XiaozhenChen <skycxz@qq.com>
2019-11-09 12:15:04 +0800

    Added glm.

--- premake5.lua
+++ premake5.lua
@@ -17,6 +17,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
 IncludeDir["ImGui"] = "Hazel/vendor/imgui"
+IncludeDir["glm"]   = "Hazel/vendor/glm"
 
 group "Dependencies"
 	include "Hazel/vendor/GLFW"
@@ -38,7 +39,10 @@ project "Hazel"
 	files
 	{
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl",
+		
 	}
 
 	includedirs
@@ -48,7 +52,8 @@ project "Hazel"
 		"%{prj.name}/src/Hazel",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 
@@ -109,7 +114,8 @@ project "Sandbox"
 	includedirs
 	{
 		"Hazel/vendor/spdlog/include",
-		"Hazel/src"
+		"Hazel/src",
+		"%{IncludeDir.glm}"
 	}
 
 	links
@@ -137,6 +143,6 @@ project "Sandbox"
 		optimize "On"
 
 	filter "configurations:Dist"
-		defines "HZ_DIST"
+		defines "HZ_DIST" 
 		runtime "Release"
 		optimize "On" 
